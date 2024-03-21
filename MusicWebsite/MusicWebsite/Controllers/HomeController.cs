d13967f309f4b50380b5b967e8b3a6c9d9981ea6
KieuCongHau <tduy7545@gmail.com>
2024-03-21 13:12:15 +0700

    #Commit first

---
+++ MusicWebsite/MusicWebsite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicWebsite.Models;
+using System.Diagnostics;
+
+namespace MusicWebsite.Controllers
+{
+    [Route("/")]
+    [Route("home")]
+    public class HomeController : Controller
+    {
+
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+
+     
+    }
+}
