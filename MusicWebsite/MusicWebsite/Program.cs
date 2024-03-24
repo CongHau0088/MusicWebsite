0f0bbebeeaefa6d42c43fc2aaa343a9d23277e59
KieuCongHau <tduy7545@gmail.com>
2024-03-24 14:32:08 +0700

    login

--- MusicWebsite/MusicWebsite/Program.cs
+++ MusicWebsite/MusicWebsite/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using MusicWebsite.Models;
+using Microsoft.AspNetCore.Identity;
+using MusicWebsite.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MusicWebsiteContextConnection") ?? throw new InvalidOperationException("Connection string 'MusicWebsiteContextConnection' not found.");
 
 // Add services to the container.
+var connectString = builder.Configuration["Connection:DefaultString"];
+builder.Services.AddDbContext<MusicWebsiteContext>(option => option.UseSqlServer(connectString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<MusicWebsiteContext>();
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
